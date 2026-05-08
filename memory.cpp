6be6445786693b42bc193e7462511f79bea177d6
angeltiff-07 <angeltiffanym31829@gmail.com>
2026-05-08 15:26:05 -0700

    Refactor MemoryManager methods and fix formatting

--- memory.cpp
+++ memory.cpp
@@ -2,39 +2,34 @@
 #include <iostream>
 using namespace std;
 
-MemoryManager::MemoryManager(int size)
-{
+MemoryManager::MemoryManager(int size) {
     totalSize = size;
     memory.assign(size, 0);
 }
 
 // First-Fit allocation
-int MemoryManager::allocate(int pid, int sizeNeeded)
-{
+int MemoryManager::allocate(int pid, int sizeNeeded) {
     int count = 0;
     int start = -1;
 
-    for (int i = 0; i < totalSize; i++)
-    {
-        if (memory[i] == 0)
-        {
-            if (count == 0)
+    for (int i = 0; i < totalSize; i++) {
+        if (memory[i] == 0) {
+            if (count == 0) {
                 start = i;
+            }
 
             count++;
 
-            if (count == sizeNeeded)
-            {
+            if (count == sizeNeeded) {
                 // allocate
-                for (int j = start; j < start + sizeNeeded; j++)
-                {
+                for (int j = start; j < start + sizeNeeded; j++) {
                     memory[j] = pid;
                 }
                 return start;
             }
         }
-        else
-        {
+            
+        else {
             count = 0;
         }
     }
@@ -42,31 +37,25 @@ int MemoryManager::allocate(int pid, int sizeNeeded)
     return -1; // allocation failed
 }
 
-// release memory
-void MemoryManager::release(int pid)
-{
-    for (int i = 0; i < totalSize; i++)
-    {
-        if (memory[i] == pid)
-        {
+// releases memory
+void MemoryManager::release(int pid) {
+    for (int i = 0; i < totalSize; i++) {
+        if (memory[i] == pid) {
             memory[i] = 0;
         }
     }
 }
 
 // debug
-void MemoryManager::display(int time)
-{
+void MemoryManager::display(int time) {
     cout << "[t=" << time << "] Memory: ";
 
     int i = 0;
-    while (i < (int)memory.size())
-    {
+    while (i < (int)memory.size()) {
         int pid = memory[i];
         int start = i;
 
-        while (i < (int)memory.size() && memory[i] == pid)
-        {
+        while (i < (int)memory.size() && memory[i] == pid) {
             i++;
         }
 
